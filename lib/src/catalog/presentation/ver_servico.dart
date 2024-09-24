cf4fc9d898c4b56fff98eb3e6c4d6af0e7c322b4
kawan Nascimento <capar.2022113tads0383@aluno.ifpi.edu.br>
2024-09-24 11:07:49 -0300

    mostar o serviço

--- lib/src/catalog/presentation/ver_servico.dart
+++ lib/src/catalog/presentation/ver_servico.dart
@@ -1,32 +1,65 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class Servico extends StatelessWidget {
- 
-  
+class Servico extends StatefulWidget {
+  @override
+  _ServicoState createState() => _ServicoState();
+}
+
+class _ServicoState extends State<Servico> {
+  Map<String, dynamic>? servicoDetails;
+
+  @override
+  void initState() {
+    super.initState();
+    // Chamamos a função que busca os detalhes do serviço ao iniciar a tela
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _getServicoDetails();
+    });
+  }
+
+  // Função para buscar os detalhes do serviço pela API
+  Future<void> _getServicoDetails() async {
+    final int? servicoId = ModalRoute.of(context)?.settings.arguments as int?;
+    if (servicoId == null) {
+      // Se o ID do serviço for nulo, não faz nada
+      return;
+    }
+
+    final String url = 'https://api.marquei.pro/api/services/$servicoId}';
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        servicoDetails = jsonDecode(response.body); // Armazena os detalhes do serviço
+      });
+    } else {
+      print('Erro ao buscar detalhes do serviço: ${response.statusCode}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          // Imagem Placeholder
-          _buildImageSection(),
-          // Informações centrais
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: _buildInfoSection(),
+      body: servicoDetails == null
+          ? Center(child: CircularProgressIndicator()) // Exibe um loading enquanto carrega
+          : Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                _buildImageSection(),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: _buildInfoSection(),
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
     );
   }
 
   Widget _buildImageSection() {
-    // String? selectedAction; // Variável para armazenar a ação selecionada
-
     return Stack(
       children: [
         Container(
@@ -34,6 +67,14 @@ class Servico extends StatelessWidget {
           decoration: BoxDecoration(
             color: Colors.grey[300],
             borderRadius: BorderRadius.circular(12),
+            image: servicoDetails != null
+                ? DecorationImage(
+                    image: NetworkImage(
+                      'https://api.marquei.pro${servicoDetails!['photo']}',
+                    ),
+                    fit: BoxFit.cover,
+                  )
+                : null,
           ),
         ),
         Positioned(
@@ -43,22 +84,13 @@ class Servico extends StatelessWidget {
           child: AppBar(
             backgroundColor: Colors.transparent,
             elevation: 0,
-            title: Text(''),
             actions: [
-              SizedBox(width: 8), // Espaço entre o dropdown e a borda
-              Text('Ações'), // Nome "Ações" ao lado esquerdo
-
-              // Dropdown para ações
               PopupMenuButton<String>(
                 icon: Icon(Icons.keyboard_arrow_down_outlined),
                 onSelected: (String value) {
-                  // Ações a serem realizadas quando o item do menu é selecionado
                   if (value == 'editar') {
-                    // Coloque aqui a lógica para editar
-                    
                     print('Editar selecionado');
                   } else if (value == 'deletar') {
-                    // Coloque aqui a lógica para deletar
                     print('Deletar selecionado');
                   }
                 },
@@ -74,11 +106,7 @@ class Servico extends StatelessWidget {
                     ),
                   ];
                 },
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10), // Raio das bordas
-                ),
-                color: Colors.white, // Cor de fundo do menu
-              ), // Espaço entre "Ações" e o ícone de editar
+              ),
             ],
           ),
         ),
@@ -91,12 +119,11 @@ class Servico extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.center,
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
-        // Título e ícone de edição
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Text(
-              'Nome do Serviço',
+              servicoDetails?['name'] ?? 'Nome do Serviço',
               style: TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
@@ -105,11 +132,10 @@ class Servico extends StatelessWidget {
           ],
         ),
         SizedBox(height: 8),
-        // Descrição do serviço
         Align(
           alignment: Alignment.centerLeft,
           child: Text(
-            'A descrição do serviço vem aqui',
+            servicoDetails?['description'] ?? 'Descrição do Serviço',
             style: TextStyle(
               color: Colors.blueGrey,
               fontSize: 16,
@@ -117,7 +143,6 @@ class Servico extends StatelessWidget {
           ),
         ),
         SizedBox(height: 16),
-        // Informações adicionais alinhadas à esquerda
         Align(
           alignment: Alignment.centerLeft,
           child: Column(
@@ -127,15 +152,7 @@ class Servico extends StatelessWidget {
                 children: [
                   Icon(Icons.access_time),
                   SizedBox(width: 8),
-                  Text('30min'),
-                ],
-              ),
-              SizedBox(height: 8),
-              Row(
-                children: [
-                  Icon(Icons.category),
-                  SizedBox(width: 8),
-                  Text('Categoria'),
+                  Text(servicoDetails?['duration'] ?? 'Duração'),
                 ],
               ),
               SizedBox(height: 8),
@@ -143,64 +160,13 @@ class Servico extends StatelessWidget {
                 children: [
                   Icon(Icons.attach_money),
                   SizedBox(width: 8),
-                  Text('R\$ 50,00'),
+                  Text('R\$ ${servicoDetails?['value'] ?? 'Preço'}'),
                 ],
               ),
             ],
           ),
         ),
-        Divider(height: 32, thickness: 1),
-        // Título dos últimos agendamentos
-        Align(
-          alignment: Alignment.centerLeft,
-          child: Text(
-            'Últimos agendamentos',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-        SizedBox(height: 8),
-        // Lista de agendamentos
-        _buildAppointmentRow(
-            'Nome do Cliente', '29/08/2024 - 11:30', 'Pendente'),
-        _buildAppointmentRow(
-            'Nome do Cliente 2', '29/08/2024 - 19:30', 'Pendente'),
       ],
-    );
-  }
-
-  Widget _buildAppointmentRow(String name, String date, String status) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                name,
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 4),
-              Text(date),
-            ],
-          ),
-          Chip(
-            label: Text(
-              status,
-              style: TextStyle(color: Colors.black),
-            ),
-            backgroundColor: Colors.yellow[500],
-            side: BorderSide.none,
-          ),
-        ],
-      ),
     );
   }
 }
